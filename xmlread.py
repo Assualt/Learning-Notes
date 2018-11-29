8152daea3eaf5e6e7825f0599820033538434c28
sunshine <1159321967@qq.com>
2018-11-29 21:42:39 +0800

    更新一个xml文件读取任意节点的函数

--- xmlread.py
+++ xmlread.py
@@ -3,46 +3,44 @@ import xml.dom.minidom as xmlread
 
 xmlconfig = xmlread.parse('./config.xml')
 
-def getxmlValue(rootnode,keyname): # such as getXmlValue(root,"string/path")
-    root=rootnode.documentElement
-    # print("begin:",root )
-    params=keyname.split('/')
-    if len(params)==1:
+
+def getxmlValue(rootnode, keyname, attr=None):  # such as getXmlValue(root,"string/path")
+    root = rootnode.documentElement
+    params = keyname.split('/')
+    if len(params) == 1:
         if len(root.getElementsByTagName(params[0])[0].childNodes) == 0:
             return ''
         else:
             return root.getElementsByTagName(params[0])[0].firstChild.data
         pass
-    for i,param in enumerate(params):
+    for i, param in enumerate(params):
         root = root.getElementsByTagName(param)
-        # print("for big",param,len(root),root,i)
         if len(root) == 0:
-            # print("the keyname is Valid")
             return ''
         else:
-            bFound=False
+            bFound = False
             for item in root:
-                searchIndex=i
+                searchIndex = i
                 if i+1 < len(params):
-                    searchIndex+=1
-                node=item.getElementsByTagName(params[searchIndex])
-                # print("for small",node,params[i])
-                if len(node)!=0:
-                    root=item
-                    bFound=True
+                    searchIndex += 1
+                node = item.getElementsByTagName(params[searchIndex])
+                if len(node) != 0:
+                    root = item
+                    bFound = True
                     break
-            if bFound==False:
-                # print("can't find such %s value"%params[searchIndex])
-                # print("root:",root)
+            if bFound == False:
                 break
         pass
     if len(root[0].childNodes) == 0:
         return ''
     else:
+        if attr != None:
+            return root[0].getAttribute(attr)
         return root[0].firstChild.data
     pass
 
-if __name__ == '__main__':
-    print(getxmlValue(xmlconfig,'user/tag'))
-    print(getxmlValue(xmlconfig,'name'))
 
+if __name__ == '__main__':
+    print(getxmlValue(xmlconfig, 'user/tag', 'vxalue'))
+    print(getxmlValue(xmlconfig, 'name'))
+    print(getxmlValue(xmlconfig, 'pss/tag', 'value'))
