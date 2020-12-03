ed5d2b463bbc3a528d0f2712389529a148a8861e
xhou <1159321967@qq.com>
2020-12-04 00:36:33 +0800

    add cmdline.hpp and demo

--- c++/tldextract/unitHttpClient.cpp
+++ c++/tldextract/unitHttpClient.cpp
@@ -1,49 +1,91 @@
 #include "httpclient.hpp"
 #include <signal.h>
-void Usage()
-{
-    std::cout << "./program [url string] [destFile string]" << std::endl;
-    exit(0);
-}
-http::HttpClient client;
+#include "../argparse/cmdline.hpp"
+#define AcceptEncoding_Default "gzip, deflate, br"
+#define AcceptLanguage_Default "zh-CN,zh;q=0.9"
+#define Accept_Default "*/*"
+#define UserAgent_Default "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+int main(int argc, char **argv){
+    cmdline::parser CommandParse;
+    CommandParse.add("help", 'h' , "This is a simple http client which support http/https");
+    CommandParse.add("version",'v', "show version of client cpp");
+    CommandParse.add<string>("url", 'u', "Input the request url", true, "");
+    CommandParse.add<string>("type", 't', "requst url type", false, "GET", cmdline::oneof<string>("GET","POST","DELETE","PUT"));
+    CommandParse.add<bool>("enable_redirect", 0, "when response of request's url contains `location` and status code is 3xx, redirect now?", false, false, cmdline::oneof<bool>(true,false));
+    CommandParse.add<bool>("verbose", 'V', "display the debug process message", false, false, cmdline::oneof<bool>(true, false));
+    CommandParse.add<string>("userAgent", 0, "set request's header userAgent", false, UserAgent_Default);
+    CommandParse.add<string>("acceptEncoding",0, "set request's header AcceptEncoding", false, AcceptEncoding_Default);
+    CommandParse.add<string>("acceptLanguage",0, "set request's header AcceptLanguage", false, AcceptLanguage_Default);
+    CommandParse.add<string>("accept", 0, "set request's header Accept Type", false, Accept_Default);
+    CommandParse.add<int>("http_version", 0, "set request version type", false, static_cast<int>(http::utils::HTTP_1_1),
+        cmdline::oneof<int>( static_cast<int>(http::utils::HTTP_1_0), static_cast<int>(http::utils::HTTP_1_1))
+    );
+    CommandParse.add<string>("cookie",0, "set request header", false, "");
+    CommandParse.add<string>("body", 0, "the post parameter", false, "");
+    CommandParse.add<string>("output",'o', "ouput file path", false, "");
+    
+    CommandParse.add<int>("logLevel", 'l', "the logger level.(0.debug, 1.info 2.warning 3.alert", false, 1, cmdline::range<int>(0, 3));
+    CommandParse.add<int>("timeout", 0, "set connection timeout(s)", false, 10, cmdline::range<int>(0, 30));
+    CommandParse.set_program_name("httpclient");
 
-static void sigint_Handler(int sig){
-    if(sig == SIGINT){
-        logger.info("get Ctrl + C Pressed.");
-        client.SaveTempFile();
-        exit(0);
-    }
-}
+    bool ok = CommandParse.parse(argc, argv);
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
-        Usage();
-    }
-    signal(SIGINT, sigint_Handler);
+    if(!ok || argc < 2){
+		std::cout << CommandParse.error() << std::endl;
+        std::cout << CommandParse.usage() << std::endl;
+    }else{
+        if(CommandParse.exist("help")){
+            std::cout << CommandParse.usage() << std::endl;
+            return 0;
+        }
+        if(CommandParse.exist("version")){
+            std::cout << "v1.0.0" << std::endl;
+            return 0;
+        }
+        std::string reqUrl = CommandParse.get<std::string>("url");
+        http::HttpClient client;
 
-    logger.setLevel(tlog::detail::DEBUG);
-    // client.setBasicAuthUserPass("xhou", "houXIN19960917");
-    client.setConnectTimeout(3);
-    client.setAcceptEncoding("gzip, deflate, br");
-    client.setAccept("*/*");
-    client.setAcceptLanguage("zh-CN,zh;q=0.9");
-    client.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
-    client.setHttpVersion(http::utils::HTTP_1_1);
-    // http::HttpResult Result = client.Get(argv[ 1 ], true, true);
-    // http::HttpResult Result = client.Post(argv[1], "requstOK", false, true);
+        int nLevel = CommandParse.get<int>("logLevel");
+        logger.setLevel(static_cast<tlog::detail::Level>(nLevel));
+        bool bRedireect = CommandParse.get<bool>("enable_redirect");
+        bool bVerbose = CommandParse.get<bool>("verbose");
+        // Make header
+        client.setAccept(CommandParse.get<string>("accept"));
+        client.setConnectTimeout(CommandParse.get<int>("timeout"));
+        client.setUserAgent(CommandParse.get<string>("userAgent"));
+        client.setAcceptEncoding(CommandParse.get<string>("acceptEncoding"));
+        client.setAcceptLanguage(CommandParse.get<string>("acceptLanguage"));
+        int httpVersion = CommandParse.get<int>("http_version");
+        client.setHttpVersion(static_cast<http::utils::HttpVersion>(httpVersion));
 
-    http::HttpResult Result = client.Get(argv[1], true, true);
+        std::string strCookie = CommandParse.get<string>("cookie");
+        if(!strCookie.empty())
+            client.setCookie(strCookie);
 
-    std::cout << "\nStatus Code:" << Result.status_code() << std::endl
+        std::string reqType = CommandParse.get<std::string>("type");
+        http::HttpResult Result;
+        if(strcasecmp(reqType.c_str(), "get") == 0){
+            Result = client.Get(reqUrl, bRedireect, bVerbose);
+        }else if( strcasecmp(reqType.c_str(), "post") == 0){
+            std::string body = CommandParse.get<string>("body");
+            if(body.empty()){
+                std::cout << "post data is empty. ignored";
+
+            }else{
+                Result = client.Get(reqUrl, bRedireect, bVerbose);
+            }
+        }
+
+        std::cout << "\nStatus Code:" << Result.status_code() << std::endl
               << "Text Size:" << Result.text().size() << std::endl
-              << "Relay Error:" << Result.error() << std::endl;
+              << "Relay Message:" << Result.error() << std::endl
+              << "Text :" << Result.text() << std::endl;
+        std::string downloadPath = CommandParse.get<string>("output");
+        if(!downloadPath.empty()){
+            client.SaveResultToFile(downloadPath);
+            logger.info("succesful save response to %s file", downloadPath);
+        }
 
-    if (argc == 3)
-    {
-        client.SaveResultToFile(argv[2]);
     }
-
     return 0;
 }
